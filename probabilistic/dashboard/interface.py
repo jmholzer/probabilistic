1adf33d39f0f30da21a3de3c5e6bcc955496ac1f
Jannic Holzer <jannic.holzer@quantumblack.com>
2022-12-04 17:44:06 +0000

    Add functions to construct app

--- probabilistic/dashboard/interface.py
+++ probabilistic/dashboard/interface.py
@@ -2,35 +2,59 @@ import streamlit as st
 import pandas as pd
 
 
+def generate_interface() -> None:
+    """Main execution path for generating the app.
+    """
+    generate_title()
+    generate_tab_group()
+
+
+def generate_title() -> None:
+    """Generate the main title of the app.
+    """
+    st.title("Probabilistic")
+
+
+def generate_tab_group() -> None:
+    """Generate the main tab group and populate with data.
+    """
+    input_tab, results_tab = st.tabs(["Input", "Results"])
+    with input_tab:
+        generate_input_tab()
+    with results_tab:
+        st.write("blank")
+
+
+def generate_input_tab() -> None:
+    """Generate content for the input page.
+    """
+    options_chain = pd.DataFrame({"strike": [], "bid": [], "ask": []})
+    uploaded_file = st.file_uploader("", key="1")
+    container = st.empty()
+    container.dataframe(options_chain, width=1024, height=386)
+
+    if uploaded_file is None:
+        st.info(f"""Upload a .csv file containing options data.""")
+    else:
+        options_chain = pd.read_csv(uploaded_file)
+
+    with container.container():
+        st.dataframe(options_chain, width=1024, height=386)
+
+
 def _max_width_():
     max_width_str = f"max-width: 1800px;"
     st.markdown(
         f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
+        <style>
+            .reportview-container .main .block-container{{
+                {max_width_str}
+            }}
+        </style>    
+        """,
         unsafe_allow_html=True,
     )
 
 
-options_chain = pd.DataFrame({"strike": [], "bid": [], "ask": []})
-st.title("Probabilistic")
-uploaded_file = st.file_uploader("", key="1")
-container = st.empty()
-container.table(options_chain)
-if uploaded_file is None:
-    st.info(
-        f"""
-                Upload a .csv file containing options data.
-                """
-    )
-    st.stop()
-else:
-    file_container = st.expander("Check your uploaded .csv")
-    options_chain = pd.read_csv(uploaded_file)
-
-with container.container():
-    st.table(options_chain)
+if __name__ == "__main__":
+    generate_interface()
