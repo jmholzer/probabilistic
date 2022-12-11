eb72a26b0f2186ab43efdf2d45ae063f69befa10
Henry Tian <tyrneh@gmail.com>
2022-12-11 23:26:40 +0000

    Add metadata input fields

--- probabilistic/dashboard/interface.py
+++ probabilistic/dashboard/interface.py
@@ -3,12 +3,14 @@ from pathlib import Path
 import pandas as pd
 import streamlit as st
 
+from typing import Optional, Dict
+
 
 def generate_interface() -> None:
     """Main execution path for generating the app.
     """
     generate_title()
-    generate_tab_group()
+    generate_body()
 
 
 def generate_title() -> None:
@@ -18,31 +20,49 @@ def generate_title() -> None:
     st.image(str(logo_path))
 
 
-def generate_tab_group() -> None:
+def generate_body() -> None:
     """Generate the main tab group and populate with data.
     """
-    input_tab, results_tab = st.tabs(["Input", "Results"])
-    with input_tab:
-        generate_input_tab()
-    with results_tab:
-        st.write("blank")
+    st.title("Security Details", anchor=None)
+    generate_input_section()
+    st.title("Result")
+    generate_results()
 
 
-def generate_input_tab() -> None:
-    """Generate content for the input page.
+def generate_input_section() -> None:
+    """Generate content for the input section.
     """
+    security_name = st.text_input("Security name")
+    c1, c2 = st.columns(2)
+    with c1:
+        current_price = st.number_input('Current price', step=0.1)
+    with c2:
+        estimate_date = st.date_input('Estimate price on')
+
     options_chain = pd.DataFrame({"strike": [], "bid": [], "ask": []})
-    uploaded_file = st.file_uploader("", key="1")
+    uploaded_file = st.file_uploader("Call options data", key="1")
+
     container = st.empty()
     container.dataframe(options_chain, width=1024, height=386)
-
     if uploaded_file is None:
-        st.info(f"""Upload a .csv file containing options data.""")
+        st.info(f"""Upload a .csv file containing call option data.""")
     else:
         options_chain = pd.read_csv(uploaded_file)
-
     with container.container():
         st.dataframe(options_chain, width=1024, height=386)
+
+    output_options = st.multiselect(
+        "Show me the following ",
+        ["PDF", "CDF"],
+        ["PDF", "CDF"]
+    )
+
+
+def generate_results(state: Optional[Dict] = None) -> None:
+    """Generate content for the results section
+    """
+    if not state:
+        st.info("Enter valid input to generate results")
 
 
 def _max_width_():
