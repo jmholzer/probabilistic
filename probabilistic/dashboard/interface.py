213e74a8cde48bfcb4f5e175f49e52f6b5aa0d85
Henry Tian <tyrneh@gmail.com>
2022-12-12 01:04:53 +0000

    Add keys to input fields, add call data to session state

--- probabilistic/dashboard/interface.py
+++ probabilistic/dashboard/interface.py
@@ -32,24 +32,28 @@ def generate_body() -> None:
 def generate_input_section() -> None:
     """Generate content for the input section.
     """
-    security_name = st.text_input("Security name")
+    security_name = st.text_input(label="Security name", key="security_name")
     c1, c2 = st.columns(2)
     with c1:
-        current_price = st.number_input('Current price', step=0.1)
+        current_price = st.number_input(
+            label="Current price",
+            step=0.1,
+            key = "current_price",
+        )
     with c2:
-        estimate_date = st.date_input('Estimate price on')
+        estimate_date = st.date_input(label='Estimate price on', key="estimate_date")
 
-    options_chain = pd.DataFrame({"strike": [], "bid": [], "ask": []})
+    st.session_state["calls"] = pd.DataFrame({"strike": [], "bid": [], "ask": []})
     uploaded_file = st.file_uploader("Call options data", key="1")
 
     container = st.empty()
-    container.dataframe(options_chain, width=1024, height=386)
+    container.dataframe(st.session_state["calls"], width=1024, height=386)
     if uploaded_file is None:
         st.info(f"""Upload a .csv file containing call option data.""")
     else:
-        options_chain = pd.read_csv(uploaded_file)
+        st.session_state["calls"] = pd.read_csv(uploaded_file)
     with container.container():
-        st.dataframe(options_chain, width=1024, height=386)
+        st.dataframe(st.session_state["calls"], width=1024, height=386)
 
     output_options = st.multiselect(
         "Show me the following ",
@@ -58,11 +62,10 @@ def generate_input_section() -> None:
     )
 
 
-def generate_results(state: Optional[Dict] = None) -> None:
+def generate_results() -> None:
     """Generate content for the results section
     """
-    if not state:
-        st.info("Enter valid input to generate results")
+    st.info("Enter valid input to generate results")
 
 
 def _max_width_():
