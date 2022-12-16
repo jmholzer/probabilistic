bf111afc11a5af31b62962d27aae5b0a55456b9a
Henry Tian <tyrneh@gmail.com>
2022-12-16 11:29:17 +0000

    Refactor interface of read method to allow passing in a DataFrame

--- probabilistic/io/reader.py
+++ probabilistic/io/reader.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from pandas import DataFrame
 
+from typing import Union
+
 
 class AbstractReader(ABC):
     """Abstract class for readers -- ingest data from a source
@@ -13,17 +15,24 @@ class AbstractReader(ABC):
         _transform_data
     """
 
-    def read(self, url: str) -> DataFrame:
+    def read(self, input_data: Union[str, DataFrame]) -> DataFrame:
         """The main API endpoint for Reader objects. Read the data at a given
         URL into a DataFrame, clean its column names and return it.
 
         Arguments:
-            url: the url to retrieve the raw data from
+            input_data: the url to retrieve the raw data from, or raw data itself
         """
-        if not url:
-            raise ValueError("url is empty or invalid")
-        raw_data = self._ingest_data(url)
-        cleaned_data = self._clean_data(raw_data)
+        if isinstance(input_data, DataFrame):
+            if input_data.empty:
+                raise ValueError("Input DataFrame contains no data")
+        elif input_data == "":
+            raise ValueError("Input url is empty")
+        elif input_data is None:
+            raise ValueError("Either an input URL or DataFrame must be specified")
+
+        if isinstance(input_data, str):
+            input_data = self._ingest_data(input_data)
+        cleaned_data = self._clean_data(input_data)
         transformed_cleaned_data = self._transform_data(cleaned_data)
         return transformed_cleaned_data
 
