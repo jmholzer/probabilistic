70381849044b5b84e39c1938a995795f1e075e78
Henry Tian <tyrneh@gmail.com>
2022-12-20 17:59:47 +0000

    Lint core

--- probabilistic/core/pdf.py
+++ probabilistic/core/pdf.py
@@ -2,10 +2,10 @@ from typing import Dict, Tuple
 
 import numpy as np
 from pandas import DataFrame
-from scipy.interpolate import interp1d
-from scipy.stats import norm
 from scipy.integrate import simps
+from scipy.interpolate import interp1d
 from scipy.optimize import fsolve
+from scipy.stats import norm
 
 from probabilistic.core.tvr import DiffTVR
 
@@ -59,7 +59,9 @@ def calculate_cdf(pdf_point_arrays: Tuple[np.array]) -> Tuple[np.array]:
         if i == 0:
             integral = 0.0 + remaining_area / 2
         else:
-            integral = simps(y=pdf_array[i - 1:i + 1], x=x_array[i - 1:i + 1]) + cdf[-1]
+            integral = (
+                simps(y=pdf_array[i - 1 : i + 1], x=x_array[i - 1 : i + 1]) + cdf[-1]
+            )
         cdf.append(integral)
     return (x_array, cdf)
 
@@ -77,7 +79,7 @@ def calculate_quartiles(cdf_point_arrays: Tuple[np.array]) -> Dict[float, float]
     return {
         0.25: fsolve(cdf_interpolated - 0.25, 0),
         0.5: fsolve(cdf_interpolated - 0.5, 0),
-        0.75: fsolve(cdf_interpolated - 0.75, 0)
+        0.75: fsolve(cdf_interpolated - 0.75, 0),
     }
 
 
@@ -141,7 +143,7 @@ def _create_pdf_point_arrays(
     vol_surface = interp1d(
         options_data.strike, options_data.iv, kind="cubic", fill_value="extrapolate"
     )
-    dx_1 = 0.05 # setting dx = 0.05 for the numerical differentiation of 1st derivative
+    dx_1 = 0.05  # setting dx = 0.05 for the numerical differentiation of 1st derivative
     X = np.arange(options_data.strike.min(), options_data.strike.max(), dx_1)
 
     # re-values call options using the BS formula, taking in as inputs S, domain, IV, and time to expiry
@@ -150,10 +152,12 @@ def _create_pdf_point_arrays(
     first_derivative_discrete = np.gradient(interpolated, X)
 
     # to speed up TVR, we increase dx and therefore reduce n
-    dx_2 = dx_1 * 10 # setting dx for the 2nd derivative to be 10x more sparse
-    X_sparse = X[0::10] # array navigation: start at 0, go to end, every 10 values
+    dx_2 = dx_1 * 10  # setting dx for the 2nd derivative to be 10x more sparse
+    X_sparse = X[0::10]  # array navigation: start at 0, go to end, every 10 values
     n_sparse = len(X_sparse)
-    first_derivative_sparse = first_derivative_discrete[0::10] # start at 0, go to end, every 10 values
+    first_derivative_sparse = first_derivative_discrete[
+        0::10
+    ]  # start at 0, go to end, every 10 values
 
     # calculate second derivative of the call options prices using TVR
     diff_tvr = DiffTVR(n_sparse, dx_2)
@@ -162,9 +166,9 @@ def _create_pdf_point_arrays(
         deriv_guess=np.full(n_sparse + 1, 0.0),
         alpha=10,
         no_opt_steps=100,
-    )    
+    )
 
-    return (X_sparse, y[:len(X_sparse)])
+    return (X_sparse, y[: len(X_sparse)])
 
 
 def _call_value(S, K, sigma, t=0, r=0):
@@ -172,7 +176,7 @@ def _call_value(S, K, sigma, t=0, r=0):
     # use np.multiply and divide to handle divide-by-zero
     with np.errstate(divide="ignore"):
         d1 = np.divide(1, sigma * np.sqrt(t)) * (
-            np.log(S / K) + (r + sigma ** 2 / 2) * t
+            np.log(S / K) + (r + sigma**2 / 2) * t
         )
         d2 = d1 - sigma * np.sqrt(t)
     return np.multiply(norm.cdf(d1), S) - np.multiply(norm.cdf(d2), K * np.exp(-r * t))
@@ -182,7 +186,7 @@ def _call_vega(S, K, sigma, t=0, r=0):
     # TODO: refactor this function (style)
     with np.errstate(divide="ignore"):
         d1 = np.divide(1, sigma * np.sqrt(t)) * (
-            np.log(S / K) + (r + sigma ** 2 / 2) * t
+            np.log(S / K) + (r + sigma**2 / 2) * t
         )
     return np.multiply(S, norm.pdf(d1)) * np.sqrt(t)
 
