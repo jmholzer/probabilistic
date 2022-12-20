341a2f716759e798bf4795e68dced737e93ce933
Jannic Holzer <jannic.holzer@quantumblack.com>
2022-12-20 17:58:03 +0000

    Add quartiles to generate_figure

--- probabilistic/graphics/matplot.py
+++ probabilistic/graphics/matplot.py
@@ -4,20 +4,16 @@ from matplotlib import pyplot
 from matplotlib.figure import Figure
 from numpy import array
 
+from probabilistic.core import calculate_quartiles
 
-def draw_figure(density_function: Tuple[array], title: Optional[str] = None) -> None:
-    """Draw a density function using matplotlib
-
-    Returns:
-        None
-    """
-    pyplot.scatter(x=density_function[0], y=density_function[1], s=0.1)
-    pyplot.title(title)
-    pyplot.show()
+pyplot.ticklabel_format(style="sci", axis="y", scilimits=(0, 0))
 
 
 def generate_figure(
-    density_function: Tuple[array], title: Optional[str] = None
+    density_function: Tuple[array],
+    title: Optional[str] = None,
+    *,
+    quartiles: Optional[bool] = False
 ) -> Figure:
     """Create a Matplotlib Figure object of a PDF
 
@@ -29,4 +25,10 @@ def generate_figure(
     fig, ax = pyplot.subplots()
     ax.scatter(x=density_function[0], y=density_function[1], s=0.1)
     ax.set_title(title)
+
+    if quartiles:
+        quartile_bounds = calculate_quartiles(density_function)
+        for k, v in quartile_bounds.items():
+            ax.axvspan(xmin=0, xmax=k, ymin=0, ymax=v)
+
     return fig
