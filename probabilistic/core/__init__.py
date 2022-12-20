be208681c77a67f80a8026881fc36384f9600769
Jannic Holzer <jannic.holzer@quantumblack.com>
2022-12-20 17:59:47 +0000

    Lint core

--- probabilistic/core/__init__.py
+++ probabilistic/core/__init__.py
@@ -1,3 +1,3 @@
-from .pdf import calculate_pdf, calculate_cdf, calculate_quartiles
+from .pdf import calculate_cdf, calculate_pdf, calculate_quartiles
 
 __all__ = ["calculate_pdf", "calculate_cdf", "calculate_quartiles"]
