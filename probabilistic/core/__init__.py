c6a1fa7bba3a992bb9d3d77c837aee4ebc1866da
Jannic Holzer <jannic.holzer@quantumblack.com>
2022-12-20 17:54:58 +0000

    Add calculate_quartiles

--- probabilistic/core/__init__.py
+++ probabilistic/core/__init__.py
@@ -1,3 +1,3 @@
-from .pdf import calculate_pdf, calculate_cdf
+from .pdf import calculate_pdf, calculate_cdf, calculate_quartiles
 
-__all__ = ["calculate_pdf", "calculate_cdf"]
+__all__ = ["calculate_pdf", "calculate_cdf", "calculate_quartiles"]
