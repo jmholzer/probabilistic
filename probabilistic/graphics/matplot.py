bed68f0935c619200c2e6dd927d9f89fe303a27b
Henry Tian <tyrneh@gmail.com>
2022-12-21 21:30:16 +0000

    Add y-axis formatting

--- probabilistic/graphics/matplot.py
+++ probabilistic/graphics/matplot.py
@@ -1,18 +1,21 @@
+from datetime import datetime
 from typing import Optional, Tuple, Union
 
 from matplotlib import pyplot
 from matplotlib.figure import Figure
-from numpy import array
+from matplotlib.ticker import MultipleLocator
+from numpy import array, linspace
 
 from probabilistic.core import calculate_quartiles
 
 
 def generate_cdf_figure(
     density_function: Tuple[array],
-    title: Optional[str] = None,
+    security_name: str,
+    estimate_date: datetime,
     *,
     current_price: Optional[Union[float, bool]] = False,
-    quartiles: Optional[bool] = False
+    quartiles: Optional[bool] = False,
 ) -> Figure:
     """Create a Matplotlib Figure object of a PDF
 
@@ -23,8 +26,19 @@ def generate_cdf_figure(
     """
     fig, ax = pyplot.subplots()
     ax.plot(density_function[0], density_function[1])
-    ax.set_title(title)
+    ax.set_title(
+        f"Cumulative Density Function of the price of"
+        f"\n{security_name} on {estimate_date}"
+    )
+
+    # add axis titles
+    ax.set_xlabel(f"Price")
+    ax.set_ylabel("Probability")
+
+    # format y-axis
     ax.ticklabel_format(style="sci", axis="y", scilimits=(0, 0))
+    ax.set_yticks(linspace(0, 1, 11))
+    ax.yaxis.set_minor_locator(MultipleLocator(0.05))
 
     if current_price:
         pass
