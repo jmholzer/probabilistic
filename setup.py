a17a3c4e6fdd44b34a19581dfb82b6bf26509b5a
Henry Tian <tyrneh@gmail.com>
2024-07-24 11:49:56 +0100

    fixed installation to install subfolders. Changed options data files to reflect new structure (get rid of bid/ask columns)

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        package_data={
+            'probabilistic.dashboard.resources': ['*.png'],
+        },
+    )
