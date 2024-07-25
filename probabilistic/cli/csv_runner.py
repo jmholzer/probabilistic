fce640d2dad9653fda5d14afe5051b83a8f26b64
Henry Tian <tyrneh@gmail.com>
2024-07-25 13:02:50 +0100

    Updated CLI package to be able to run probabilistic without using web interface, and save results to csv

--- probabilistic/cli/csv_runner.py
+++ probabilistic/cli/csv_runner.py
@@ -1,8 +1,9 @@
-from probabilistic.core import calculate_pdf
+from probabilistic.core import calculate_pdf, calculate_cdf
 from probabilistic.io import CSVReader
+import pandas as pd
 
 
-def run(input_csv_path: str, current_price: float, days_forward: int) -> None:
+def run(input_csv_path: str, current_price: float, days_forward: int, output_csv_path: str) -> None:
     """Run probabilistic using the data from a CSV file as input
 
     Args:
@@ -10,10 +11,25 @@ def run(input_csv_path: str, current_price: float, days_forward: int) -> None:
         current_price: the current price of the security
         days_forward: the number of days in the future to estimate the
             price probability density at
+        output_csv_path: the path to save the output csv results 
 
     Returns:
-        None
+        the output csv results containing 3 columns: Price, PDF, CDF
     """
     reader = CSVReader()
     options_data = reader.read(input_csv_path)
-    pdf = calculate_pdf(options_data, current_price, days_forward)
+    pdf_point_arrays = calculate_pdf(options_data, current_price, days_forward)
+    cdf_point_arrays = calculate_cdf(pdf_point_arrays)
+
+    priceP, densityP = pdf_point_arrays
+    priceC, densityC = cdf_point_arrays
+
+    # Convert results to DataFrame
+    df = pd.DataFrame({
+        'Price': priceP,
+        'PDF': densityP,
+        'CDF': densityC
+    })
+
+    # Write the DataFrame to a CSV file
+    df.to_csv(output_csv_path, index=False)
